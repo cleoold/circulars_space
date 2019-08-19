d2cfe397b943737e3e8050a605262fb90a2b7750
midori <cleoold@gmail.com>
2019-08-20 01:39:39 +0800

    refactered, changed style. added acleration feature
    preparing to support spawning multiple boxes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # uniform circular motion demo
 
-uniform circular motion demo page with auto div scaling!
+uniform (probably not so uniform because it supports linear acceleration) circular motion demo page with auto div scaling!
+
 see demo at https://cleoold.github.io/uniform_circular_motion/index.html
--- js/movingbox.js
+++ js/movingbox.js
@@ -2,17 +2,40 @@
     uniform circular motion demo! ()cleoold
 */
 
+// page layout
+
+var pBody = document.querySelector('body')
+var pHtml = document.querySelector('html')
+
+// background change
+window.addEventListener('load', () => {
+        var colors = ['#30542c', '#232170', '#530a80', '#870c6d', '#873a0b']
+        //var colorsLight = ['#4c9144', '#514db0', '#a06bc2', '#ab609b']
+        let i = 0
+        setInterval(() => {
+            if (++i == 5) i = 0
+            pBody.style.backgroundColor = colors[i]
+            pHtml.style.backgroundColor = colors[i]
+        }, 5000)
+    })
+
+// container of moving block
+
 var container = document.querySelector('div#container')
-var containerWidthText = document.querySelector('#container-width')
-var movingBoxXText = document.querySelector('#box-x')
-var movingBoxYText = document.querySelector('#box-y')
-var setPeriodText = document.querySelector('#set-period')
-var setRadiusText = document.querySelector('#set-radius')
+var containerWidthText = document.querySelector('.container-width')
 
 var containerLength = 0
 
 function floatToPx(num) {
     return String(num) + 'px'
+}
+
+function wrongInput(elem) {
+    let o = elem.style.backgroundColor
+    elem.style.backgroundColor = '#c98d8d'
+    setTimeout(() => {
+        elem.style.backgroundColor = o
+    }, 400)
 }
 
 function onChangeContainerSize(e) {
@@ -35,38 +58,91 @@ function Block(initOffset = 0, initPeriod = 0, initRadius = 0) {
     this.period = (t) => this.initPeriod
     this.omega = (t) => 2 * Math.PI / this.period(t)
     this.radius = (t) => this.initRadius
-    this.yieldX = (t) => this.radius() * Math.cos(this.omega() * t - this.offset)
-    this.yieldY = (t) => this.radius() * Math.sin(this.omega() * t - this.offset)
+    this.yieldX = (t) => this.radius(t) * Math.cos(this.omega(t) * t - this.offset)
+    this.yieldY = (t) => this.radius(t) * Math.sin(this.omega(t) * t - this.offset)
 }
 
-var block1 = new Block()
-var timer = 0
-var blockElement = document.querySelector('div#moving-obj')
-window.addEventListener('load', (e) => {
-    setPeriodText.value = 5
-    setRadiusText.value = containerLength / 4
-})
+// moving block area
 
-setInterval(() => {
-    timer += 40
-    var realTimer = timer / 1000
-    var x = floatToPx(block1.yieldX(realTimer) + containerLength / 2)
-    var y = floatToPx(block1.yieldY(realTimer) + containerLength / 2)
-    blockElement.style.left = x
-    movingBoxXText.value = x
-    blockElement.style.top = y
-    movingBoxYText.value = y
-}, 40)
+function BlockInMotion(id) {
+    var block1 = new Block()
+    var maxRadiusInContainer = 0
+    var timer = 0
+    var blockElement = document.querySelector(`div#moving-obj-${id}`)
+    var movingBoxXText = document.querySelector(`#info-container-${id} .box-x`)
+    var movingBoxYText = document.querySelector(`#info-container-${id} .box-y`)
+    var setPeriodText = document.querySelector(`#info-container-${id} .set-period`)
+    var setRadiusText = document.querySelector(`#info-container-${id} .set-radius`)
+    var setPeriodLDeltaText = document.querySelector(`#info-container-${id} .set-period-change`)
+    var setRadiusLDeltaText = document.querySelector(`#info-container-${id} .set-radius-change`)
+    // set default values of the circle
+    window.addEventListener('load', (e) => {
+        maxRadiusInContainer = containerLength / 2
+        setPeriodText.value = 5
+        setRadiusText.value = containerLength / 4
+        setPeriodLDeltaText.value = 0
+        setRadiusLDeltaText.value = 0
+        blockElement.style.left = '50%'
+        blockElement.style.top = '50%'
+    })
+    // ON RESIZE, if object exceeds the container, keep it at the boundary
+    window.addEventListener('resize', (e) => {
+        maxRadiusInContainer = containerLength / 2
+        if (block1.initRadius > maxRadiusInContainer) {
+            block1.initRadius = maxRadiusInContainer
+            block1.initRadius = maxRadiusInContainer
+        }
+        setRadiusText.value = block1.initRadius
+    })
 
-document.querySelector('#set-attr-go').addEventListener('click', (e) => {
-    if (Math.abs(parseFloat(setRadiusText.value)) <= containerLength / 2) {
-        block1.initPeriod = parseFloat(setPeriodText.value)
-        block1.initRadius = parseFloat(setRadiusText.value)
-    } else {
-        let o = setRadiusText.style.backgroundColor
-        setRadiusText.style.backgroundColor = '#c98d8d'
-        setTimeout(() => {
-            setRadiusText.style.backgroundColor = o
-        }, 400)
-    }
-})
+    // renders the moving object every frame, also updates the X, Y- axis text box
+    setInterval(() => {
+        timer += 40
+        var realTimer = timer / 1000
+        var x = block1.yieldX(realTimer) + maxRadiusInContainer
+        var y = block1.yieldY(realTimer) + maxRadiusInContainer
+        // ON ACCELERATION, if object exceeds the container, keep it at the boundary
+        if (x < 0 || x > containerLength || y < 0 || y > containerLength) {
+            block1.initRadius = maxRadiusInContainer
+            block1.radius = (t) => block1.initRadius
+            block1.initPeriod = 0
+            block1.period = (t) => block1.initPeriod
+            setRadiusLDeltaText.value = 0
+            setPeriodText.value = 0
+        }
+        blockElement.style.left = floatToPx(x)
+        movingBoxXText.value = floatToPx(x)
+        blockElement.style.top = floatToPx(y)
+        movingBoxYText.value = floatToPx(y)
+
+        // move these somewhere else
+        //setPeriodText.value = block1.period(realTimer)
+        //setRadiusText.value = block1.radius(realTimer)
+    }, 40)
+
+    var enterBtn = document.querySelector(`#info-container-${id} .set-attr-go`)
+    enterBtn.addEventListener('click', (e) => {
+        if (Math.abs(parseFloat(setRadiusText.value)) <= maxRadiusInContainer) {
+            block1.initPeriod = parseFloat(setPeriodText.value)
+            block1.initRadius = parseFloat(setRadiusText.value)
+            enterBtn.value = 'Change!'
+        } else {
+            wrongInput(setRadiusText)
+        }
+        if (!setPeriodLDeltaText.value) setPeriodLDeltaText.value = 0
+        if (!setRadiusLDeltaText.value) setRadiusLDeltaText.value = 0
+        if (Math.abs(parseFloat(setRadiusLDeltaText.value)) <= maxRadiusInContainer) {
+            var periodLD = parseFloat(setPeriodLDeltaText.value)
+            block1.period = (t) => block1.initPeriod + t * periodLD
+            var radiusLD = parseFloat(setRadiusLDeltaText.value)
+            block1.radius = (t) => block1.initRadius + t * radiusLD
+        } else {
+            wrongInput(setRadiusLDeltaText)
+        }
+    })
+}
+
+var circular1 = new BlockInMotion('1')
+
+
+
